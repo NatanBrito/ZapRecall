ef49b99ccc1c5c282906d1de2efb45c343732bdc
Natan__Brito <natanismaelbrito@gmail.com>
2022-03-18 09:43:30 -0300

    salvando pq ta bugado

--- src/componentes/BotaoIniciar.js
+++ src/componentes/BotaoIniciar.js
@@ -1,18 +1,14 @@
 import react from "react"
 export default function BotaoIniciar({texto}){
     const[inicioRecall,setInicioRecall]=react.useState(true)
-      if(inicioRecall === true){
-          return(
-            <>
-            <BotaoIniciar  texto="Iniciar Recall!"/> 
-           </>)
-          
-      } else {
-          return (<h1>aa</h1>)
+      if(inicioRecall){
+  return(
+    <div className="alinhandoBotao">
+    <button onClick={()=>{ setInicioRecall(false)}} className="configBotao">{texto}</button>
+    </div>
+  )
+  } else {
+          return <h1 >aa</h1>
       }
-// return(
-//     <div className="alinhandoBotao">
-//     <button onClick={()=>{ setInicioRecall(false)}} className="configBotao">{texto}</button>
-//     </div>
-// )
+
 }
